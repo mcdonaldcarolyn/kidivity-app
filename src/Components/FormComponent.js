61cc79ba03dc74fbabc5f6f3a1a117ccd96dfbb2
Carolyn Whelpley <carolynwhelpley@Carolyns-MacBook-Air.local>
2019-09-12 09:11:05 -0400

    created handle changes for parts of state in from

--- src/Components/FormComponent.js
+++ src/Components/FormComponent.js
@@ -12,6 +12,16 @@ export default class FormComponent extends Component{
             name: event.target.value
         })
     }
+    handleAddressChange = event => {
+        this.setState({
+            address: event.target.value
+        })
+    }
+    handleDescriptionChange = event =>{
+        this.setState({
+            description: event.target.value
+        })
+    }
 
     handeOnSumbit = event => {
         this.props.addItem(this.state.name, this.state.address, this.state.description)
@@ -22,8 +32,8 @@ export default class FormComponent extends Component{
             <div>
                 <form onSubmit={this.props.addItem}>
                     <input type="text" placeholder="name" onChange={this.handleNameChange}/>
-                    <input type='text' placeholder="address"/>
-                    <input type="text" placeholder="description"/>
+                    <input type='text' placeholder="address" onChange={this.handleAddressChange}/>
+                    <input type="text" placeholder="description" onChange={this.handleDescriptionChange}/>
                     <button type="submit">create</button>
                 </form>
             </div>
