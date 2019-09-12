adc8a531027c424555a77483b2dba14c68004603
Carolyn Whelpley <carolynwhelpley@Carolyns-MacBook-Air.local>
2019-09-12 08:39:02 -0400

    add form component to activities page

--- src/Components/ActivityPageComponent.js
+++ src/Components/ActivityPageComponent.js
@@ -1,8 +1,13 @@
 import React, {Component} from 'react';
 
+
 export default class ActivityPageComponent extends Component{
     
     render(){
-        
+        return (
+            <div>
+                <FormComponent addItem={this.addItem}/>
+            </div>
+        )
     }
 }
