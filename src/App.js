d7c37fb558f24550f748add24f34881bce0e0dc6
Carolyn Whelpley <carolynwhelpley@Carolyns-Air.fios-router.home>
2019-09-12 15:54:02 -0400

    add react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import './App.css';
 import ActivityPageComponent from'./Components/ActivityPageComponent';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+
 export default class App extends Component {
   render(){
   return (
     <div className='App'>
       
          <h1> Welcome to Kidivity</h1>
+
         <ActivityPageComponent/>
     </div>
   );
