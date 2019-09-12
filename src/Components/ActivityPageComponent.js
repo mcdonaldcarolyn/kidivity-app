9ebc0489adaedec0422ae06543d320469d41a5cc
Carolyn Whelpley <carolynwhelpley@Carolyns-MacBook-Air.local>
2019-09-12 09:50:44 -0400

    add list component to render in activities page

--- src/Components/ActivityPageComponent.js
+++ src/Components/ActivityPageComponent.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import FormComponent from "./FormComponent"
+import ListComponent from './ListComponent';
 
 export default class ActivityPageComponent extends Component{
     constructor(){
@@ -10,10 +11,12 @@ export default class ActivityPageComponent extends Component{
     }
     addItem = (name, address, description) => {
         console.log ("got to addItem in activiies page")
+        
     }
     render() {
         return (
             <div>
+                <ListComponent activites={this.state.activities}/>
                 <FormComponent addItem={this.addItem}/>
             </div>
         )
