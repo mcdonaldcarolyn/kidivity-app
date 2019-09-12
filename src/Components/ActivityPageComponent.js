7a1c8a1a6b3781421be8734666258f3613bd92bd
Carolyn Whelpley <carolynwhelpley@Carolyns-MacBook-Air.local>
2019-09-12 08:48:32 -0400

    add constructor to activities page

--- src/Components/ActivityPageComponent.js
+++ src/Components/ActivityPageComponent.js
@@ -1,8 +1,14 @@
 import React, {Component} from 'react';
-
+import FormComponent from "./FormComponent"
+import { thisExpression } from '@babel/types';
 
 export default class ActivityPageComponent extends Component{
-    
+    constructor(){
+        super()
+        this.state ={
+            activities: []
+        }
+    }
     render(){
         return (
             <div>
