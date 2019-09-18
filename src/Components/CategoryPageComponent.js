8abd3d9bcb37ad725c6b58973a3e22842d0c1264
Carolyn Whelpley <carolynwhelpley@Carolyns-Air.fios-router.home>
2019-09-18 08:38:34 -0400

    change list to category

---
+++ src/Components/CategoryPageComponent.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import FormComponent from "./FormComponent"
+import ListComponent from '../container/ListComponent';
+
+export default class CategoriesPageComponent extends Component {
+
+    render(){
+        return(
+            <ListComponent/>
+        )
+    }
+}
