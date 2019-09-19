ced6057ae59cc935d7eb72305a6e2a474c1b87ae
Carolyn Whelpley <carolynwhelpley@Carolyns-Air.fios-router.home>
2019-09-19 19:00:58 -0400

    build up activity show page

--- src/container/ActivityPageComponent.js
+++ src/container/ActivityPageComponent.js
@@ -3,5 +3,14 @@ import {connect} from 'react-redux';
 import {Link } from 'react-router-dom';
 
 export class ActivityPageComponent extends Component{
-    
+    render(){
+        const {id } = this.props.match.parms;
+        const activityId = parseInt(id, 10);
+        const activity = this.props.activities.find(activity => activity.id === activityId);
+        return (
+            <div>
+                
+            </div>
+        )
+    }
 }
