9003d3a9a81f64ed78411b7ffe7f1ef416dfddf0
Carolyn Whelpley <carolynwhelpley@Carolyns-Air.fios-router.home>
2019-09-19 18:36:17 -0400

    fix typo on the link

--- src/Components/ActivityListComponent.js
+++ src/Components/ActivityListComponent.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
- 
+import { Link } from 'react-router-dom';
+
 export default class ActivityListComponent extends Component{
     renderActivities = () => {
         return this.props.activities.map((activity, idx) => {
             return (
                 <li key= {idx}>
-                    <Link to "/activities/{activity.id}">{activity.name}</Link>
+                    <Link to="/activities/{activity.id}">{activity.name}</Link>
                 </li>
             );
             });
