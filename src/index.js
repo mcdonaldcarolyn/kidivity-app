4bcea2b20280d9465a8a6f5f3a22b76230e1703e
Carolyn Whelpley <carolynwhelpley@Carolyns-Air.fios-router.home>
2019-09-21 15:38:16 -0400

    add thunk to index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// import { createStore } from 'redux';
+ import { createStore } from 'redux';
+import thunk from 'redux-thunk';
+import {Provider } from "react-redux";
 
-//const store = createStore()
+const middlewares = [thunk];
+const store = createStore()
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
