82390083a5b6bcd920edfe6d862fc74cde9b74e2
Carolyn Whelpley <carolynwhelpley@Carolyns-Air.fios-router.home>
2019-09-21 15:48:58 -0400

    build out store and add provider to index

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
- import { createStore } from 'redux';
+ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import {Provider } from "react-redux";
 
 const middlewares = [thunk];
-const store = createStore()
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    manageActivities,
+    initialState,
+    applyMiddleware(...middlewares)
+);
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>,
+ document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
