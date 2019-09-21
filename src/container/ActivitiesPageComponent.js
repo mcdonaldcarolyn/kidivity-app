60d397c3686aedace0f8c1cce2f0292239434031
Carolyn Whelpley <carolynwhelpley@Carolyns-Air.fios-router.home>
2019-09-21 15:09:56 -0400

    added activiesListC to Activities page

--- src/container/ActivitiesPageComponent.js
+++ src/container/ActivitiesPageComponent.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
 import ActivityFormComponent from "../Components/ActivityFormComponent"
 import ListComponent from '../Components/ActivityListComponent';
+import ActivityListComponent from '../Components/ActivityListComponent';
 
 export default class ActivitiesPageComponent extends Component {
 render(){
     return (
-        <ActivityFormComponent/>
+        <div>
+        <h1>All fun kidivities</h1>
+        <ActivityListComponent activities={this.props.activities}/>
+        <Link to="/activities-new">Add new Activity</Link>
+        </div>
     )
 }
 
