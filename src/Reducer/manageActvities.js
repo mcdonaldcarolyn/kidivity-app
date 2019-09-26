b92981b7e4407f6d77b98ab296dc56687f038cdd
Carolyn Whelpley <carolynwhelpley@Carolyns-Air.fios-router.home>
2019-09-26 16:55:05 -0400

    add fetch to reducer

--- src/Reducer/manageActvities.js
+++ src/Reducer/manageActvities.js
@@ -12,7 +12,12 @@ export default function manageActivities(state = initialState, action) {
         ...state,
         activities: [...state.activities, action.activity]
       };
-    
+    case FETCH_ACTIVITIES_SUCCESS:
+      return {
+        ...state,
+        activities: action.activities
+      };
+
     default:
       return state;
   }
