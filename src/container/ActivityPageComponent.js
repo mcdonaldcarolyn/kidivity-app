85a17309881d4ec840cdd0bcd3906f9c83a5313d
Carolyn Whelpley <carolynwhelpley@Carolyns-Air.fios-router.home>
2019-10-07 13:37:29 -0400

    Interim checkin

--- src/container/ActivityPageComponent.js
+++ src/container/ActivityPageComponent.js
@@ -3,14 +3,16 @@ import {connect} from 'react-redux';
 
 export class ActivityPageComponent extends Component{
     render() {
-        const categories = this.props.categories || [];
-        const activities = this.props.activities || [];
+        
+        
         const { id } = this.props.match.params;
         const activityId = parseInt(id, 10);
         const categoryId = parseInt(id, 10);
         debugger
-        const activity = activities.find(activity => activity.id === activityId);
-        const category = categories.find(category => categoryId === category.id)
+        const activities = this.props.activities || [];
+        const activity = activities.find(activity => activity.id === activityId) || {};
+        const categories = this.props.categories || [];
+        const category = categories.find(category => categoryId === category.id) || {}
         return (
             <div>
                 <h1>{activity.name}</h1>
--- src/container/HomePageComponent.js
+++ src/container/HomePageComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import CategoryListComponent from "../Components/CategoryListComponent";
-
+import background from '../images/background.png'
 import { connect } from "react-redux";
 
 export class HomePageComponent extends Component {
@@ -10,9 +10,15 @@ export class HomePageComponent extends Component {
       textAlign: 'center',
       fontFamily: "Bradley Hand, cursive"
     };
-    
+    const sectionStyle = {
+      width: '100%',
+      height: '400px',
+      backgroundImage: "url("+ {background} + ")"
+
+    }
+
     return (
-      <div>
+      <div style={sectionStyle}>
         <h2 style ={headerstyle}>Welcome to Kidivity</h2>
         <h4> Where you can create a master list of Kid
        acivities!</h4>
