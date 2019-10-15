3a6e3fb87f4166398ce3a3216802f52e902a3fb0
Carolyn Whelpley <carolynwhelpley@Carolyns-Air.fios-router.home>
2019-10-15 06:09:41 -0400

    clean up dropdown menu

--- src/Components/MenuComoponent.js
+++ src/Components/MenuComoponent.js
@@ -39,10 +39,7 @@ export default class MenuComponent extends Component {
              
             </NavDropdown>
           </Nav>
-          {/* <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
-          </Form> */}
+          
         </Navbar.Collapse>
       </Navbar>
     );
